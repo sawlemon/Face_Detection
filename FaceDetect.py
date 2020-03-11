e004ef2a255d66e916a548c6f808b0e2d47529ec
Sala-Khaliff-Jr <33852997+Sala-Khaliff-Jr@users.noreply.github.com>
2020-03-11 21:44:10 +0530

    removed images

--- FaceDetect.py
+++ FaceDetect.py
@@ -2,29 +2,19 @@ import cv2
 cascPath = "haarcascade_frontalface_default.xml"
 faceCascade = cv2.CascadeClassifier(cascPath)
 video_capture = cv2.VideoCapture(0)
-
 while True:
     ret, frame = video_capture.read()
-
-    frame = cv2.imread("faces.jpg")
-    # frame = cv2.imread("cg_faces.jpg")
-
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-
     faces = faceCascade.detectMultiScale(
         gray,
         scaleFactor=1.1,
         minNeighbors=2,
         minSize = (30, 30),
-
     )
     for (x, y, w, h) in faces:
         cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
-    
     cv2.imshow('Video', frame)
-
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
-
 video_capture.release()
 cv2.destroyAllWindows()
