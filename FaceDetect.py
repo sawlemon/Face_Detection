77d462314ae583096cd503659f07a5e0d9544244
sawlemon <33852997+sawlemon@users.noreply.github.com>
2024-01-18 00:41:50 +0530

    added commets

--- FaceDetect.py
+++ FaceDetect.py
@@ -1,19 +1,28 @@
 import cv2
 cascPath = "haarcascade_frontalface_default.xml"
 faceCascade = cv2.CascadeClassifier(cascPath)
+# 0 means default device
 video_capture = cv2.VideoCapture(0)
+
 while True:
+    #  ret has true or false based on if the frame is returned. frame has the actual frame data
     ret, frame = video_capture.read()
+    
+    # Convert color to gray
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    
     faces = faceCascade.detectMultiScale(
         gray,
         scaleFactor=1.1,
         minNeighbors=2,
         minSize = (30, 30),
     )
+    
     for (x, y, w, h) in faces:
         cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
+    
     cv2.imshow('Video', frame)
+    
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 video_capture.release()
